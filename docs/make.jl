3193e841893c0a0ff6a245a44bd3253674ab0e92
Scott McIntire <scottrsm@gmail.com>
2024-04-03 16:36:51 -0400

    Added github doc and test actions.

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, Cards
+using Cards
+import Pkg
 
-makedocs(sitename="Cards Docs")
+Pkg.add("Documenter")
+using Documenter
 
+makedocs(
+	sitename = "Cards",
+	format = Documenter.HTML(),
+	modules = [Cards]
+	)
+
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/Cards.jl.git"
+	)
